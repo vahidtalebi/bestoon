cd5d5e0525adb8d6fb58592c1ca9fbca5f5882d4
vahidtalebi <vxd791@gmail.com>
2021-06-03 13:05:46 +0430

    Expance was created

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Expance
 # Register your models here.
+
+admin.site.register(Expance)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Expance(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
