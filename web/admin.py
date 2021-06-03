ccfede0da19717ce0953d319c28d572826b58309
vahidtalebi <vxd791@gmail.com>
2021-06-03 17:06:14 +0430

    Nicome was created

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Expance
+from .models import Expense, Income
 # Register your models here.
 
-admin.site.register(Expance)
+admin.site.register(Expense)
+admin.site.register(Income)
